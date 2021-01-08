d0ffd872edfc2af3d854143b26a30b9f92aa4b0f
Yann Burrett <yann.let@gmail.com>
2021-01-08 15:51:39 +0000

    Vehicle Template

---
+++ Cars/VehicleTemplate.gd
@@ -0,0 +1,9 @@
+extends VehicleBody
+
+export var Player_ID = 0
+export var car_name = ""
+
+export var max_speed = 20
+export var throttle = 210000
+export var max_steer = 15
+export var max_health = 100
