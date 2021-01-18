63a2361033784c6d4e0a9074416b11b09a463016
Yann Burrett <yann.let@gmail.com>
2021-01-18 15:17:24 +0000

    Many Driveable Cars

--- Cars/VehicleTemplate.gd
+++ Cars/VehicleTemplate.gd
@@ -11,13 +11,13 @@ export var max_health = 100
 
 func _physics_process(delta):
 	engine_force = 0
-	if Input.is_action_pressed("Player_1_Forward"):
+	if Input.is_action_pressed("Player_"+str(Player_ID)+"_Forward"):
 		if linear_velocity.length() < max_speed:
 			engine_force = throttle * delta
-	if Input.is_action_pressed("Player_1_Back"):
+	if Input.is_action_pressed("Player_"+str(Player_ID)+"_Back"):
 		if linear_velocity.length() < max_speed:
 			engine_force = -throttle * delta
 		
-	steering +=(Input.get_action_strength("Player_1_Left") - Input.get_action_strength("Player_1_Right")) * delta
+	steering +=(Input.get_action_strength("Player_"+str(Player_ID)+"_Left") - Input.get_action_strength("Player_"+str(Player_ID)+"_Right")) * delta
 	steering = clamp(steering, -max_steer, max_steer)
 	steering = lerp(steering, 0, 0.1)
