774209cbcc4c869ac7354d0b8523338e3e2a086f
Yann Burrett <yann.let@gmail.com>
2021-01-18 15:16:47 +0000

    A Driveable Car

--- Cars/VehicleTemplate.gd
+++ Cars/VehicleTemplate.gd
@@ -7,3 +7,17 @@ export var max_speed = 20
 export var throttle = 210000
 export var max_steer = 15
 export var max_health = 100
+
+
+func _physics_process(delta):
+	engine_force = 0
+	if Input.is_action_pressed("Player_1_Forward"):
+		if linear_velocity.length() < max_speed:
+			engine_force = throttle * delta
+	if Input.is_action_pressed("Player_1_Back"):
+		if linear_velocity.length() < max_speed:
+			engine_force = -throttle * delta
+		
+	steering +=(Input.get_action_strength("Player_1_Left") - Input.get_action_strength("Player_1_Right")) * delta
+	steering = clamp(steering, -max_steer, max_steer)
+	steering = lerp(steering, 0, 0.1)
